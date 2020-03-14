f014cc12358d20cb89ecee5b6aca48e515e9c98c
Polaryti <45695029+Polaryti@users.noreply.github.com>
2020-03-14 18:29:14 +0100

    Torrent

--- sc02-Forest.py
+++ sc02-Forest.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import RandomizedSearchCV, cross_val_score, train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.preprocessing import Normalizer, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -16,6 +16,27 @@ dictio_i = {0: 'RESIDENTIAL\n',
     4: 'OTHER\n',
     5: 'RETAIL\n',
     6: 'AGRICULTURE\n'}
+
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
 
 ###
 variables_per_class = []
@@ -36,16 +57,32 @@ X_train, X_test, y_train, y_test = train_test_split(eq_data[:, 1:55], eq_data[:,
 ###
 
 ###
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
+rf = RandomForestClassifier()
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = ['auto', 'sqrt']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+rf_random.fit(X_train, y_train)
 ###
 
 ###
-y_pred = clf.predict(X_test)
+y_pred = rf_random.predict(X_test)
 print(classification_report(y_test, y_pred))
+print(rf_random.best_params_)
 
-final_predictions = clf.predict(predict[:, 1:])
-with open('res-forest.txt', 'w') as file:
+final_predictions = rf_random.predict(predict[:, 1:])
+with open(r'Outpur\forestH.txt', 'w') as file:
     with open(r'Data\Estimar_UH2020.csv', 'r') as read:
         for i in range(len(final_predictions)):
             file.write('{}|{}'.format(read.readline().split('|')[0], dictio_i[final_predictions[i]]))
---
+++ torrent.py
@@ -0,0 +1,114 @@
+# Datos equilibrados al por mayor
+# Requilibrado sin mezcla de muestras de entrenamiento en muestras de test 
+
+import xgboost as xgb
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.ensemble import RandomForestClassifier
+
+test_avg = 0.2
+
+avg_prediction = {}
+
+dictio_i = {0: 'RESIDENTIAL\n',
+    1: 'INDUSTRIAL\n',
+    2: 'PUBLIC\n',
+    3: 'OFFICE\n',
+    4: 'OTHER\n',
+    5: 'RETAIL\n',
+    6: 'AGRICULTURE\n'}
+
+def most_frequent(List): 
+    return max(set(List), key = List.count) 
+
+##
+data = np.genfromtxt(r'Data\Modelar_UH2020.csv', delimiter = ',')
+predict = np.genfromtxt(r'Data\Estimar_UH2020.csv', delimiter='|')
+
+for iteration in range(20):
+    np.random.shuffle(data)
+    variables_per_class = []
+    for i in range(7):         
+        variables_per_class.append([])
+    for label in data:
+        variables_per_class[int(label[55])].append(label)
+
+    ## Data normalizado al por mayor
+    eq_data_major = []
+    for i in range(90173):
+        for j in range(7):
+            max_sample = len(variables_per_class[j]) - int(len(variables_per_class[j]) * test_avg)
+            eq_data_major.append(variables_per_class[j][i % max_sample])
+
+    eq_test_major = []
+    for i in range(7):
+        min_sample = len(variables_per_class[i]) - int(len(variables_per_class[i]) * test_avg)
+        for j in range(int(len(variables_per_class[i]) * test_avg)):
+            eq_test_major.append(variables_per_class[i][min_sample + j])
+
+    eq_data_major = np.array(eq_data_major)
+    np.random.shuffle(eq_data_major)
+    eq_test_major = np.array(eq_test_major)
+    np.random.shuffle(eq_test_major)
+
+    X_train_major = eq_data_major[:, 1:55]
+    y_train_major = eq_data_major[:, 55]
+    X_test_major = eq_test_major[:, 1:55]
+    y_test_major = eq_test_major[:, 55]
+
+    ## Data normalizada al por menor
+    eq_data_menor = []
+    for i in range(338):
+        for j in range(7):
+            eq_data_menor.append(variables_per_class[j][i])
+    eq_data_menor = np.array(eq_data_menor)
+    X_train_menor, X_test_menor, y_train_menor, y_test_menor = train_test_split(eq_data_menor[:, 1:55], eq_data_menor[:, 55], test_size = test_avg)
+    ##
+
+
+    ##
+    bst = xgb.XGBClassifier()
+    bst.fit(X_train_menor, y_train_menor)
+    y_pred = bst.predict(X_test_menor)
+    print(confusion_matrix(y_test_menor, y_pred))
+    print(classification_report(y_test_menor, y_pred))
+    final_predictions = bst.predict(predict[:, 1:])
+    with open(r'Output\torrent\torrent_0{}_XGB_m.txt'.format(iteration), 'w') as file:
+        with open(r'Data\Estimar_UH2020.csv', 'r') as read:
+            for i in range(len(final_predictions)):
+                line = read.readline().split('|')
+                file.write('{}|{}'.format(line[0], dictio_i[final_predictions[i]]))
+                if (line[0] not in avg_prediction):
+                    avg_prediction[line[0]] = [int(final_predictions[i])]
+                else:
+                    avg_prediction[line[0]].append(int(final_predictions[i]))
+    ##
+    ##
+    bst = RandomForestClassifier()
+    bst.fit(X_train_menor, y_train_menor)
+    y_pred = bst.predict(X_test_menor)
+    print(confusion_matrix(y_test_menor, y_pred))
+    print(classification_report(y_test_menor, y_pred))
+    final_predictions = bst.predict(predict[:, 1:])
+    with open(r'Output\torrent\torrent_0{}_rndf_m.txt'.format(iteration), 'w') as file:
+        with open(r'Data\Estimar_UH2020.csv', 'r') as read:
+            for i in range(len(final_predictions)):
+                line = read.readline().split('|')
+                file.write('{}|{}'.format(line[0], dictio_i[final_predictions[i]]))
+                if (line[0] not in avg_prediction):
+                    avg_prediction[line[0]] = [int(final_predictions[i])]
+                else:
+                    avg_prediction[line[0]].append(int(final_predictions[i]))
+    ##
+
+## Evaluación global
+with open(r'Output\torrent\torrent_DEBUG.txt', 'w') as debug:
+    for item in avg_prediction.items():
+        debug.write(str(item))
+with open(r'Data\Estimar_UH2020.csv', 'r') as read:
+    with open(r'Output\torrent\torrent_FINAL.txt', 'w') as file:
+        for line in read.readlines():
+            line = line.split('|')
+            file.write('{}|{}'.format(line[0], dictio_i[most_frequent(avg_prediction[line[0]])]))
+## 
