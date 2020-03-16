94efb287dd3524e27e70b8ddddfad6c85fda33ff
Polaryti <45695029+Polaryti@users.noreply.github.com>
2020-03-16 11:11:58 +0100

    Torrent

--- torrent.py
+++ torrent.py
@@ -28,36 +28,13 @@ def most_frequent(List):
 data = np.genfromtxt(r'Data\Modelar_UH2020.csv', delimiter = ',')
 predict = np.genfromtxt(r'Data\Estimar_UH2020.csv', delimiter='|')
 
-for iteration in range(50):
+for iteration in range(30):
     np.random.shuffle(data)
     variables_per_class = []
     for i in range(7):         
         variables_per_class.append([])
     for label in data:
         variables_per_class[int(label[55])].append(label)
-
-    ## Data normalizado al por mayor
-    eq_data_major = []
-    for i in range(90173):
-        for j in range(7):
-            max_sample = len(variables_per_class[j]) - int(len(variables_per_class[j]) * test_avg)
-            eq_data_major.append(variables_per_class[j][i % max_sample])
-
-    eq_test_major = []
-    for i in range(7):
-        min_sample = len(variables_per_class[i]) - int(len(variables_per_class[i]) * test_avg)
-        for j in range(int(len(variables_per_class[i]) * test_avg)):
-            eq_test_major.append(variables_per_class[i][min_sample + j])
-
-    eq_data_major = np.array(eq_data_major)
-    np.random.shuffle(eq_data_major)
-    eq_test_major = np.array(eq_test_major)
-    np.random.shuffle(eq_test_major)
-
-    X_train_major = eq_data_major[:, 1:55]
-    y_train_major = eq_data_major[:, 55]
-    X_test_major = eq_test_major[:, 1:55]
-    y_test_major = eq_test_major[:, 55]
 
     ## Data normalizada al por menor
     eq_data_menor = []
@@ -105,6 +82,73 @@ for iteration in range(50):
                 else:
                     avg_prediction[line[0]].append(int(final_predictions[i]))
     ##
+
+for iteration in range(4):
+    np.random.shuffle(data)
+    variables_per_class = []
+    for i in range(7):         
+        variables_per_class.append([])
+    for label in data:
+        variables_per_class[int(label[55])].append(label)
+
+    ## Data normalizado al por mayor
+    eq_data_major = []
+    for i in range(90173):
+        for j in range(7):
+            max_sample = len(variables_per_class[j]) - int(len(variables_per_class[j]) * test_avg)
+            eq_data_major.append(variables_per_class[j][i % max_sample])
+
+    eq_test_major = []
+    for i in range(7):
+        min_sample = len(variables_per_class[i]) - int(len(variables_per_class[i]) * test_avg)
+        for j in range(int(len(variables_per_class[i]) * test_avg)):
+            eq_test_major.append(variables_per_class[i][min_sample + j])
+
+    eq_data_major = np.array(eq_data_major)
+    np.random.shuffle(eq_data_major)
+    eq_test_major = np.array(eq_test_major)
+    np.random.shuffle(eq_test_major)
+
+    X_train_major = eq_data_major[:, 1:55]
+    y_train_major = eq_data_major[:, 55]
+    X_test_major = eq_test_major[:, 1:55]
+    y_test_major = eq_test_major[:, 55]
+
+    ##
+    bst = xgb.XGBClassifier()
+    bst.fit(X_train_major, y_train_major)
+    y_pred = bst.predict(X_test_major)
+    # print(confusion_matrix(y_test_menor, y_pred))
+    # print(classification_report(y_test_menor, y_pred))
+    print('{}_XGB_M - {}'.format(iteration, accuracy_score(y_test_major, y_pred)))
+    final_predictions = bst.predict(predict[:, 1:])
+    with open(r'Output\torrent\torrent_0{}_XGB_M.txt'.format(iteration), 'w') as file:
+        with open(r'Data\Estimar_UH2020.csv', 'r') as read:
+            for i in range(len(final_predictions)):
+                line = read.readline().split('|')
+                file.write('{}|{}'.format(line[0], dictio_i[final_predictions[i]]))
+                if (line[0] not in avg_prediction):
+                    avg_prediction[line[0]] = [int(final_predictions[i])]
+                else:
+                    avg_prediction[line[0]].append(int(final_predictions[i]))
+    ##
+    ##
+    bst = RandomForestClassifier(n_estimators = 400, min_samples_split = 5, min_samples_leaf = 1, max_features = 'auto', max_depth = 60, bootstrap = True)
+    bst.fit(X_train_major, y_train_major)
+    y_pred = bst.predict(X_test_major)
+    # print(confusion_matrix(y_test_menor, y_pred))
+    # print(classification_report(y_test_menor, y_pred))
+    print('{}_rndf_M - {}'.format(iteration, accuracy_score(y_test_major, y_pred)))
+    final_predictions = bst.predict(predict[:, 1:])
+    with open(r'Output\torrent\torrent_0{}_rndf_M.txt'.format(iteration), 'w') as file:
+        with open(r'Data\Estimar_UH2020.csv', 'r') as read:
+            for i in range(len(final_predictions)):
+                line = read.readline().split('|')
+                file.write('{}|{}'.format(line[0], dictio_i[final_predictions[i]]))
+                if (line[0] not in avg_prediction):
+                    avg_prediction[line[0]] = [int(final_predictions[i])]
+                else:
+                    avg_prediction[line[0]].append(int(final_predictions[i]))
     
 
 ## Evaluación global
