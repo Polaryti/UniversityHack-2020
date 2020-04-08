dae99c8d77754b0ce1447c442c5380f8415e8786
Polaryti <45695029+Polaryti@users.noreply.github.com>
2020-04-08 18:44:39 +0200

    Auto-MachineLearning
    Implementado el Auto-MachineLearning, falta ejecutarlo

--- autosklearnV1.0.py
+++ autosklearnV1.0.py
@@ -1,18 +1,43 @@
-"""A classification example using mlbox."""
 from mlbox.preprocessing import Reader
 from mlbox.preprocessing import Drift_thresholder
 from mlbox.optimisation import Optimiser
 from mlbox.prediction import Predictor
+import random
+
+
+data = []
+with open(r'Data\Modelar_UH2020.txt') as read_file:
+    _ = read_file.readline()
+    for line in read_file.readlines():
+        data.append(line)
+
+random.shuffle(data)
+
+with open(r'Data\askl_train.csv', 'w') as write_file:
+    write_file.write('X|Y|Q_R_4_0_0|Q_R_4_0_1|Q_R_4_0_2|Q_R_4_0_3|Q_R_4_0_4|Q_R_4_0_5|Q_R_4_0_6|Q_R_4_0_7|Q_R_4_0_8|Q_R_4_0_9|Q_R_4_1_0|Q_G_3_0_0|Q_G_3_0_1|Q_G_3_0_2|Q_G_3_0_3|Q_G_3_0_4|Q_G_3_0_5|Q_G_3_0_6|Q_G_3_0_7|Q_G_3_0_8|Q_G_3_0_9|Q_G_3_1_0|Q_B_2_0_0|Q_B_2_0_1|Q_B_2_0_2|Q_B_2_0_3|Q_B_2_0_4|Q_B_2_0_5|Q_B_2_0_6|Q_B_2_0_7|Q_B_2_0_8|Q_B_2_0_9|Q_B_2_1_0|Q_NIR_8_0_0|Q_NIR_8_0_1|Q_NIR_8_0_2|Q_NIR_8_0_3|Q_NIR_8_0_4|Q_NIR_8_0_5|Q_NIR_8_0_6|Q_NIR_8_0_7|Q_NIR_8_0_8|Q_NIR_8_0_9|Q_NIR_8_1_0|AREA|GEOM_R1|GEOM_R2|GEOM_R3|GEOM_R4|CONTRUCTIONYEAR|MAXBUILDINGFLOOR|CADASTRALQUALITYID|CLASE\n')
+    for line in data[:80000]:
+        line = line.split('|')
+        line = line[1:]
+        write_file.write('{}'.format('|'.join(line)))
+
+with open(r'Data\askl_test.csv', 'w') as write_file_a:
+    write_file_a.write('X|Y|Q_R_4_0_0|Q_R_4_0_1|Q_R_4_0_2|Q_R_4_0_3|Q_R_4_0_4|Q_R_4_0_5|Q_R_4_0_6|Q_R_4_0_7|Q_R_4_0_8|Q_R_4_0_9|Q_R_4_1_0|Q_G_3_0_0|Q_G_3_0_1|Q_G_3_0_2|Q_G_3_0_3|Q_G_3_0_4|Q_G_3_0_5|Q_G_3_0_6|Q_G_3_0_7|Q_G_3_0_8|Q_G_3_0_9|Q_G_3_1_0|Q_B_2_0_0|Q_B_2_0_1|Q_B_2_0_2|Q_B_2_0_3|Q_B_2_0_4|Q_B_2_0_5|Q_B_2_0_6|Q_B_2_0_7|Q_B_2_0_8|Q_B_2_0_9|Q_B_2_1_0|Q_NIR_8_0_0|Q_NIR_8_0_1|Q_NIR_8_0_2|Q_NIR_8_0_3|Q_NIR_8_0_4|Q_NIR_8_0_5|Q_NIR_8_0_6|Q_NIR_8_0_7|Q_NIR_8_0_8|Q_NIR_8_0_9|Q_NIR_8_1_0|AREA|GEOM_R1|GEOM_R2|GEOM_R3|GEOM_R4|CONTRUCTIONYEAR|MAXBUILDINGFLOOR|CADASTRALQUALITYID\n')
+    for line in data[80000:]:
+        line = line.split('|')
+        last = len(line) - 1
+        line = line[1:last]
+        write_file_a.write('{}\n'.format('|'.join(line)))       
+
 
 # Paths to the train set and the test set.
-paths = ["train_classification.csv", "test_classification.csv"]
+paths = [r'Data\askl_train.csv', r'Data\askl_test.csv']
 # Name of the feature to predict.
 # This columns should only be present in the train set.
-target_name = "Survived"
+target_name = "CLASE"
 
 # Reading and cleaning all files
 # Declare a reader for csv files
-rd = Reader(sep=',')
+rd = Reader(sep='|')
 # Return a dictionnary containing three entries
 # dict["train"] contains training samples withtout target columns
 # dict["test"] contains testing elements withtout target columns
