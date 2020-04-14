447dc602ac5871693ef264cbe9d6ab4ba756b04e
Asicoder <asicoderofficial@gmail.com>
2020-04-14 21:03:29 +0200

    Feature eng 1

--- not_random_test_generator.py
+++ not_random_test_generator.py
@@ -4,7 +4,7 @@ import numpy as np
 import math
 from sklearn.model_selection import train_test_split
 
-def dividir_dataset(df, train_percentage=0.8, test_percentage=0.2, randomized=False, residential_perc=14605, industrial_perc=1473, public_perc=1433, retail_perc=897, office_perc=678, other_perc=1361, agriculture_perc=200):
+def dividir_dataset(df, train_percentage=0.8, test_percentage=0.2, randomized=False, residential_perc=3000, industrial_perc=1400, public_perc=900, retail_perc=800, office_perc=500, other_perc=700, agriculture_perc=150):
     np.random.seed(7)
     if train_percentage + test_percentage != 1:
         print("El dataset debe emplearse entero, los porcentajes de train y test deben sumar 1.")
@@ -22,5 +22,11 @@ def dividir_dataset(df, train_percentage=0.8, test_percentage=0.2, randomized=Fa
         for i in range(1,7):
             df_list[0] = df_list[0].append(df_list[i], ignore_index=True)
         df = pd.concat([df, df_list[0]]).drop_duplicates(keep=False)
+        dfres = df.loc[df['CLASS'] == 0].sample(n=6000)
+        dfothers = df.loc[df['CLASS'] != 0]
+        df_train = dfres.append(dfothers, ignore_index=True)
         #Devuelve una tupla, en 0 el train y en 1 el test.
-        return df.sample(frac=1), df_list[0].sample(frac=1)
+        for i in range(7):
+            print('Class ' + str(i) + ' train: ' + str(df_train.loc[df_train['CLASS'] == i].shape[0]))
+            print('Class ' + str(i) + ' test: ' + str(df_list[0].loc[df_list[0]['CLASS'] == i].shape[0]))
+        return df_train.sample(frac=1), df_list[0].sample(frac=1)
